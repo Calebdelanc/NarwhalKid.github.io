aa88706aa269917ae93e1fb215c03076f7917f6c
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-03-09 18:32:26 -0500

    Create js.js

---
+++ games/js.js
@@ -0,0 +1,11 @@
+var version = 1
+var latest = version
+var current = version
+var element = document.getElementById("text");
+
+if current == latest {
+  element.innerHTML = "You are on the latest version!";
+}
+else {
+    element.innerHTML = "You are not on the latest version. Please go <a href="https://tinyurl.com/nknew"> to update to the latest version.";
+}
